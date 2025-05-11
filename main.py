d1181d8a3c33d59ca2ab1d56a8fc633cc4023bc2
Teertha007 <teertha.sarker.3@gmail.com>
2025-05-12 03:52:01 +0600

    Add YAML extraction functionality for tour data

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import requests
 import selectorlib
 
 
-
 URL = "http://programmer100.pythonanywhere.com/tours/"
 
 def scrape(url):
@@ -15,7 +14,25 @@ def extract_data(source):
     values = extractor.extract(source)["tours"]
     return values
 
+def send_email():
+    print("Email sent successfully!")
+
+def store(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted + "\n")
+
+def read_file(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
+
 if __name__ == "__main__":
     scrape_data =scrape(URL)
     extracted =extract_data(scrape_data)
     print(extracted)
+
+    content = read_file(extracted)
+    if extracted != "No upcoming tours":
+        if extracted not in content:
+            store(extracted)
+            send_email()
+
